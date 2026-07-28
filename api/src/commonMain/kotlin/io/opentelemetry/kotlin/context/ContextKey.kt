003281290a4e51e7034d8cdb1e18af806e019da9
Jamie Lynch <fractalwrench@gmail.com>
2026-07-28 10:24:17 +0100

    mark Context API as stable

--- api/src/commonMain/kotlin/io/opentelemetry/kotlin/context/ContextKey.kt
+++ api/src/commonMain/kotlin/io/opentelemetry/kotlin/context/ContextKey.kt
@@ -1,11 +1,9 @@
 package io.opentelemetry.kotlin.context
 
-import io.opentelemetry.kotlin.ExperimentalApi
 import io.opentelemetry.kotlin.ThreadSafe
 
 /**
  * A key that identifies a value in a [Context].
  */
-@ExperimentalApi
 @ThreadSafe
 public interface ContextKey<T>
